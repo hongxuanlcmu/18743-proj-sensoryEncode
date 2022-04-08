a53ac2cbd9745a223f4b44ff0bb76599f7148e85
ZhengfanCMU <zhengfaz@andrew.cmu.edu>
2022-04-07 21:09:09 -0500

    add nonlinear intensity translation to input encoding

--- lab1/utils.py
+++ lab1/utils.py
@@ -26,19 +26,38 @@ class PosNeg(object):
 class IntensityTranslation(object):
     def __init__(self, gamma_time):
         self.gamma_time = gamma_time
-        pass
+        # https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+        self.gamma8 = torch.Tensor(
+        [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+         1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+         2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+         5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+         10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+         17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+         25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+         37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+         51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+         69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+         90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+        115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+        144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+        177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+        215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255])
 
     def __call__(self, tensor):
         # Use finer granularity if input max isn't too large
         maxt                                    = torch.max(tensor)
-        # renormalize tensor to use the finer granularity
-        tensor = tensor / maxt
-        # copy this to use as output
-        # original tensor used for compare
+
         spike_tensor = tensor.clone()
+        # renormalize spike_tensor to use the finer granularity
+        spike_tensor = (spike_tensor / maxt * (self.gamma8.size()[0] - 1)).long()
         # delay: 0-gamma -> value: 0-maxt
         # granularity is maxt/gamma
+        spike_tensor = (self.gamma8.size()[0]) - self.gamma8[spike_tensor]
+        spike_tensor = spike_tensor / len(self.gamma8)
         spike_tensor = self.gamma_time - spike_tensor * self.gamma_time
+        
         # spike_tensor[spike_tensor > (self.gamma_time - 1)] = float('Inf')
         return spike_tensor
 
